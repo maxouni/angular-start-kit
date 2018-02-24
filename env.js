dd8473417a783e0eb7ca106b45c68127e129df09
Rie <rie.albert@gmail.com>
2018-02-25 01:43:24 +0300

    Added a script that applies environment variables

--- env.js
+++ env.js
@@ -5,7 +5,7 @@ const filename = __dirname + '/src/environments/environment.ts';
 var environments = fs.readFileSync(filename, 'utf8');
 
 Object.keys(process.env).forEach(function (key) {
-  environments = environments.replace(key, process.env[key]);
+  environments = environments.replace('{' + key + '}', process.env[key]);
 });
 
 fs.writeFileSync(filename, environments, function(err) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,11 +8,11 @@ export const environment = {
 
   /* https://firebase.google.com/ */
   firebase: {
-    apiKey: 'FIREBASEawdawdAPI/usr/local/bin/nodeKEY',
-    authDomain: 'FIREBASE_AUTH_DOMAIN',
-    databaseURL: 'FIREBASE_DATABASE_URL',
-    projectId: 'FIREBASE_PROJECT_ID',
-    storageBucket: 'FIREBASE_STORAGE_BUCKET',
-    messagingSenderId: 'FIREBAESE_SENDER_ID'
+    apiKey: '{FIREBASE_API_KEY}',
+    authDomain: '{FIREBASE_AUTH_DOMAIN}',
+    databaseURL: '{FIREBASE_DATABASE_URL}',
+    projectId: '{FIREBASE_PROJECT_ID}',
+    storageBucket: '{FIREBASE_STORAGE_BUCKET}',
+    messagingSenderId: '{FIREBAESE_SENDER_ID}'
   }
 };
